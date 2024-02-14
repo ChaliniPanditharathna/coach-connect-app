16d2ebfd435bf0451f53c3c1b0de4676c0a0ea1b
Chalini Panditharathna <panditharathnac@student.douglascollege.ca>
2024-02-13 23:41:12 -0800

    initial commit

---
+++ src/main/java/com/coachconnect/coachconnectapp/CoachConnectAppApplication.java
@@ -0,0 +1,13 @@
+package com.coachconnect.coachconnectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachConnectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachConnectAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coachconnect/coachconnectapp/CoachConnectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachconnect.coachconnectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachConnectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
