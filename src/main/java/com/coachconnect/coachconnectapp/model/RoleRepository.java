1ea9e73769cda014f29b7614d398a646f2bc7c04
Chalini Panditharathna <panditharathnac@student.douglascollege.ca>
2024-02-20 17:53:11 -0800

    User Registreation, Login, Logout, Role based authentication

---
+++ src/main/java/com/coachconnect/coachconnectapp/model/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.coachconnect.coachconnectapp.model;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+	
+	Optional<Role> findByName(EnumRole name);
+}
