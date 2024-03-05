ec16b0b386f815af7022cc4bc276a34a9beade1c
Sandaru-Bandara <bandaraa@student.douglascollege.ca>
2024-03-04 18:28:40 -0800

    RatingRepository.java added

--- src/main/java/com/coachconnect/coachconnectapp/model/Rating.java
+++ src/main/java/com/coachconnect/coachconnectapp/model/Rating.java
@@ -2,6 +2,7 @@ package com.coachconnect.coachconnectapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,12 +25,12 @@ public class Rating {
 	private double points;
 	
 
-	@ManyToOne
-    @JoinColumn(name = "client_id")
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "client_id", nullable = false)
     private User client;
 	
-	@ManyToOne
-    @JoinColumn(name = "instructor_id")
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "instructor_id", nullable = false)
     private User instructor;
 	
 	
