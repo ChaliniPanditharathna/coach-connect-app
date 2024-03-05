cd0d5c206a03f13d1ba1d2547523d59f649fcba4
nelumkc <rangebandaramuc@student.douglascollege.ca>
2024-03-04 19:21:54 -0800

    Appointment approve,reject and reschedule controller updated

--- src/main/java/com/coachconnect/coachconnectapp/model/AppointmentRepository.java
+++ src/main/java/com/coachconnect/coachconnectapp/model/AppointmentRepository.java
@@ -3,11 +3,13 @@ package com.coachconnect.coachconnectapp.model;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 	List<Appointment> findAll();
     List<Appointment> findByStatus(EnumStatus status);
     List<Appointment> findByClientId(Long clientId);
-   // List<Appointment> findByInstructorId(Long instructorId);
+   // List<Appointment> findByInstructorId(@Param("instructorId") Long instructorId);
+    
    
 }
