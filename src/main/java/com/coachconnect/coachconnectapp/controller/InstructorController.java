b75d1fa4788b1de7424e16e8a0a82547cdb0c107
nelumkc <rangebandaramuc@student.douglascollege.ca>
2024-04-10 21:55:59 -0700

    get instructor id by user

--- src/main/java/com/coachconnect/coachconnectapp/controller/InstructorController.java
+++ src/main/java/com/coachconnect/coachconnectapp/controller/InstructorController.java
@@ -2,16 +2,19 @@ package com.coachconnect.coachconnectapp.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coachconnect.coachconnectapp.model.Client;
 import com.coachconnect.coachconnectapp.model.Instructor;
 import com.coachconnect.coachconnectapp.model.InstructorRepository;
 import com.coachconnect.coachconnectapp.response.InstructorResponseDto;
@@ -52,5 +55,17 @@ public class InstructorController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	
+	@GetMapping("/instructor/{userId}")
+	public ResponseEntity<Instructor> getAllInstructors(@PathVariable("userId") long userId) {
+
+		Optional<Instructor> instructor = instructorRepository.findInstructorByUserId(userId);
+		if (instructor.isPresent()) {
+			return new ResponseEntity<>(instructor.get(), HttpStatus.OK);
+		}
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
